6572efd89eabe5bb24c5b75d95f2fb4e1e24c268
Iuliia Mukhamadiarova <64098113+iuliialanchuk@users.noreply.github.com>
2023-01-17 12:58:34 +0100

    add Location table

--- application/models.py
+++ application/models.py
@@ -1,6 +1,7 @@
 
-from . import db
+from application import db
 
+import requests
 
 class Saver:
     def save(self):
@@ -12,8 +13,19 @@ class User(db.Model, Saver):
     id = db.Column(db.Integer, primary_key = True)
     nickname = db.Column(db.String(64), unique = True)
     email = db.Column(db.String(120), unique = True)
-    city = db.Column(db.String(51))
+    city = db.Column(db.String(51), db.ForeignKey('location.city'))
     age = db.Column(db.Integer)
 
     def __repr__(self):
         return '<User %r> from <%r>' % (self.nickname, self.city)
+
+
+class Location(db.Model, Saver):
+    id = db.Column(db.Integer, primary_key=True)
+    city = db.Column(db.String(50), db.ForeignKey('user.city'), unique=True)
+    latitude = db.Column(db.Float)
+    longitude = db.Column(db.Float)
+    citizens = db.relationship('User', backref='citizen')
+
+    def __repr__(self):
+        return '<Location %r - latitude %r and longitude %r>' % (self.city, self.latitude, self.longitude)
