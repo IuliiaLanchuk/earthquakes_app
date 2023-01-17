eaffe171ced0a4257fb204b69c8c646bca5709a3
Iuliia Mukhamadiarova <64098113+iuliialanchuk@users.noreply.github.com>
2023-01-17 14:09:55 +0100

    add Location table with correct FK

--- application/models.py
+++ application/models.py
@@ -1,5 +1,5 @@
 
-from . import db
+from application import db
 
 
 class Saver:
@@ -12,8 +12,17 @@ class User(db.Model, Saver):
     id = db.Column(db.Integer, primary_key = True)
     nickname = db.Column(db.String(64), unique = True)
     email = db.Column(db.String(120), unique = True)
-    city = db.Column(db.String(51))
+    city = db.Column(db.String(51), db.ForeignKey('location.city'))
     age = db.Column(db.Integer)
 
     def __repr__(self):
         return '<User %r> from <%r>' % (self.nickname, self.city)
+
+
+class Location(db.Model, Saver):
+    city = db.Column(db.String(50),  primary_key=True)
+    latitude = db.Column(db.Float)
+    longitude = db.Column(db.Float)
+
+    def __repr__(self):
+        return '<Location %r - latitude %r and longitude %r>' % (self.city, self.latitude, self.longitude)
